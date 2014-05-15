40ef79c3fc0a7491efa78a9ba4114f4a3bfcac1c
Hannah Fouasnon <hanelifou@gmail.com>
2014-05-15 03:11:33 -0700

    add some lightweight dev tools

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  // configurable paths
+  var yeomanConfig = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  try {
+    yeomanConfig.app = require('./bower.json').appPath || yeomanConfig.app;
+  } catch (e) {}
+
+  grunt.initConfig({
+    yeoman: yeomanConfig,
+    connect: {
+      server: {
+        options: {
+          debug: true,
+          livereload: true,
+          port: 9000
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('server', [
+    'groc',
+    'connect:server:keepalive'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# Maybe Capital 
+## Site is hosted on github. 
+This means anything you push here goes live
+
+## Dev Tools
+There's a standardish bower/npm/grunt setup going on here.  You don't need to use it, but if you have a standard node setup and run
+
+``` sh
+npm install
+```
+
+you can then run 
+
+``` sh
+grunt server
+```
+
+This will give you a lightweight server available at (localhost:9000)[http://localhost:9000]
